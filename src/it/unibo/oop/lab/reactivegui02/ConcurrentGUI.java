612e25c66bb591715ea4896c3cd94f3d8a542554
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-12-09 18:12:38 +0100

    fix: remove unused import

--- src/it/unibo/oop/lab/reactivegui02/ConcurrentGUI.java
+++ src/it/unibo/oop/lab/reactivegui02/ConcurrentGUI.java
@@ -2,8 +2,6 @@ package it.unibo.oop.lab.reactivegui02;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.lang.reflect.InvocationTargetException;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -71,7 +69,6 @@ public class ConcurrentGUI extends JFrame {
         private int counter;
 
         @Override
-        @SuppressWarnings("PMD.UselessQualifiedThis")
         public void run() {
             while (!this.stop) {
                 try {
