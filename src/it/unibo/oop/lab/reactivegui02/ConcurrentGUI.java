e5e608878282cae39e671c8737a8d30f67aa7272
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-12-09 16:49:14 +0100

    feat: created ConcurrentGUI class as View

---
+++ src/it/unibo/oop/lab/reactivegui02/ConcurrentGUI.java
@@ -0,0 +1,21 @@
+package it.unibo.oop.lab.reactivegui02;
+
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+/**
+ * Reactive GUI for ex. 02.
+ */
+public class ConcurrentGUI {
+
+}
