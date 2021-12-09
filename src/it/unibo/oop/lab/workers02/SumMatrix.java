cf5a111bb0770a126e75c4429cb59f542c568fb4
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-12-09 22:03:34 +0100

    feat: add abstract method to work with stream and parallel stream

--- src/it/unibo/oop/lab/workers02/SumMatrix.java
+++ src/it/unibo/oop/lab/workers02/SumMatrix.java
@@ -8,10 +8,30 @@ package it.unibo.oop.lab.workers02;
 public interface SumMatrix {
 
     /**
+     * Return the sum of {@link matrix} elements.
+     * 
      * @param matrix
      *            an arbitrary-sized matrix
      * @return the sum of its elements
      */
     double sum(double[][] matrix);
+    
+    /**
+     * Return the sum of {@link matrix} elements using streams.
+     * 
+     * @param matrix
+     *            an arbitrary-sized matrix
+     * @return the sum of its elements
+     */
+    double sumWithStream(double[][] matrix);
+    
+    /**
+     * Return the sum of {@link matrix} elements using parallel streams.
+     * 
+     * @param matrix
+     *            an arbitrary-sized matrix
+     * @return the sum of its elements
+     */
+    double sumWithParallelStream(double[][] matrix);
 
 }
