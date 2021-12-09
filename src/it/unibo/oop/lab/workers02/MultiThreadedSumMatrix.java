4cee46692cedb7f6a242f479db5c3e9e9d3e7077
Giorgio Fantilli <giorgio.fantilli@studio.unibo.it>
2021-12-09 19:05:26 +0100

    feat: create MultiThreadedSumMatrix class

---
+++ src/it/unibo/oop/lab/workers02/MultiThreadedSumMatrix.java
@@ -0,0 +1,17 @@
+package it.unibo.oop.lab.workers02;
+
+/**
+ * A Class defining the multi-threaded sum for a matrix.
+ */
+public class MultiThreadedSumMatrix implements SumMatrix {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public double sum(final double[][] matrix) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
